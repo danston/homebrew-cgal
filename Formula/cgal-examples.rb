791af60646dc072a8a0d5436ce2314efda47c8e6
Dmitry Anisimov <danston@ymail.com>
2019-07-17 17:42:06 +0200

    cleanup

--- Formula/cgal-examples.rb
+++ Formula/cgal-examples.rb
@@ -1,23 +1,21 @@
 class CgalExamples < Formula
-  desc "Precompiled CGAL examples."
-  homepage "https://github.com/CGAL/cgal"
-  url "https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.14/CGAL-4.14.tar.xz"
-  sha256 "59464b1eaee892f2223ba570a7642892c999e29524ab102a6efd7c29c94a29f7"
+  desc "Precompiled CGAL examples with the sources."
+  homepage ""
+  url ""
+  sha256 ""
   version "4.14"
 
-  depends_on "cmake" => [:build]
-  depends_on "cgal"
+  bottle do
+    cellar :any
+    sha256 "" => :mojave
+  end
+
   depends_on "boost"
   depends_on "eigen"
   depends_on "gmp"
   depends_on "mpfr"
 
   def install
-    args = std_cmake_args + %W[
-      -DCMAKE_BUILD_TYPE=Release
-    ]
-    system "cmake", ".", *args
-    system "make", "install"
   end
 
   test do
