aca5df94e614bd770f416a45c9a466d7cbeac3ae
Dmitry Anisimov <danston@ymail.com>
2019-07-18 14:17:15 +0200

    lastools

---
+++ Formula/lastools.rb
@@ -0,0 +1,15 @@
+class Lastools < Formula
+  desc "LAS Tools"
+  homepage "https://rapidlasso.com/LAStools"
+  url "https://github.com/danston/cgal-brew/raw/master/lastools.tar.gz"
+  sha256 ""
+  version "1.0"
+
+  def install
+    system "true"
+  end
+
+  test do
+    system "true"
+  end
+end
