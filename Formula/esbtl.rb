88c0020a1d0969d321a1b36a8d6dd50d8f22fa2c
Dmitry Anisimov <danston@ymail.com>
2019-07-18 14:00:21 +0200

    cleanup

--- Formula/esbtl.rb
+++ Formula/esbtl.rb
@@ -3,9 +3,10 @@ class Esbtl < Formula
   homepage "http://esbtl.sourceforge.net"
   url "https://github.com/danston/cgal-brew/raw/master/esbtl.tar.gz"
   sha256 ""
+  version "1.0"
 
   def install
-    # include.install Dir["include/*"]
+    system "true"
   end
 
   test do
