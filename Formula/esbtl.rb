da0e7b295d5df0e49bc27e825bbe632220c86b64
Dmitry Anisimov <danston@ymail.com>
2019-07-18 14:02:17 +0200

    cleanup

--- Formula/esbtl.rb
+++ Formula/esbtl.rb
@@ -2,11 +2,11 @@ class Esbtl < Formula
   desc "Easy Structural Biology Template Library"
   homepage "http://esbtl.sourceforge.net"
   url "https://github.com/danston/cgal-brew/raw/master/esbtl.tar.gz"
-  sha256 ""
+  sha256 "7b728ae51419bc8cda918f4770e68ef08fd6053df2144af1cb052479633af9a9"
   version "1.0"
 
   def install
-    system "true"
+    include.install Dir["include/*"]
   end
 
   test do
