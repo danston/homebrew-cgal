730668eb308e71e14dc92dc5e65158e1f2a129e4
Dmitry Anisimov <danston@ymail.com>
2019-07-19 16:10:20 +0200

    update

--- Formula/cgal-examples@4.14.rb
+++ Formula/cgal-examples@4.14.rb
@@ -1,9 +1,11 @@
-class CgalExamples < Formula
-  desc "Computational Geometry Algorithms Library: Examples"
+class CgalExamplesAT414 < Formula
+  desc "Computational Geometry Algorithms Library: Examples Version 4.14"
   homepage "https://www.cgal.org/"
   url "https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.14/CGAL-4.14.tar.xz"
   sha256 "59464b1eaee892f2223ba570a7642892c999e29524ab102a6efd7c29c94a29f7"
   version "4.14"
+
+  keg_only :versioned_formula
 
   depends_on "cmake" => [:build, :test]
   depends_on "boost"
@@ -19,6 +21,7 @@ class CgalExamples < Formula
   depends_on "opencv@2"
   depends_on "danston/cgal/esbtl"
   depends_on "danston/cgal/lastools"
+  depends_on "danston/cgal/cgal@4.14"
 
   def install
     args = std_cmake_args + %W[
