a0595435be0c6c173e76d6c2ed38cb2eef7c0644
Dmitry Anisimov <danston@ymail.com>
2019-07-19 16:52:22 +0200

    update

--- Formula/cgal-examples@4.14.rb
+++ Formula/cgal-examples@4.14.rb
@@ -26,14 +26,11 @@ class CgalExamplesAT414 < Formula
   def install
     args = std_cmake_args + %W[
       -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON
+      -DCMAKE_INSTALL_NAME_DIR="/usr/local/opt/cgal@4.14/lib"
       -DCMAKE_BUILD_TYPE="Release"
       -DWITH_Eigen3=ON
       -DWITH_examples=ON
       -DBUILD_TESTING=ON
-      -DCMAKE_MACOSX_RPATH=1
-      -DCMAKE_INSTALL_RPATH="/usr/local/opt/cgal@4.14/lib"
-      -DCMAKE_BUILD_RPATH="/usr/local/opt/cgal@4.14/lib"
-      -DCMAKE_PREFIX_PATH="#{prefix}"
       -DQt5_DIR="/usr/local/opt/qt/lib/cmake/Qt5"
       -DOpenCV_DIR="/usr/local/opt/opencv@2/share/OpenCV"
     ]
