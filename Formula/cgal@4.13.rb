dc2d10e236743318e6e7d9dc95356ec7992ce56b
Dmitry Anisimov <danston@ymail.com>
2019-07-23 18:32:15 +0200

    update

--- Formula/cgal@4.13.rb
+++ Formula/cgal@4.13.rb
@@ -4,6 +4,13 @@ class CgalAT413 < Formula
   url "https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.13/CGAL-4.13.tar.xz"
   sha256 "3e3dd7a64febda58be54c3cbeba329ab6a73b72d4d7647ba4931ecd1fad0e3bc"
 
+  bottle do
+    root_url "https://dl.bintray.com/danston/bottles-cgal"
+    cellar :any
+    rebuild 4
+    sha256 "8e7875ca2c877c56fb01b39454ad311f6fa9019b0119937a900d56d59a370cc5" => :mojave
+  end
+
   keg_only :versioned_formula
 
   depends_on "cmake" => [:build, :test]
