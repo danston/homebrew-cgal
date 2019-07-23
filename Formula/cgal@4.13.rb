60cc3e25dfe52d50de7eaa9bf6960b8b7fe38e6f
Dmitry Anisimov <danston@ymail.com>
2019-07-23 15:56:14 +0200

    update

--- Formula/cgal@4.13.rb
+++ Formula/cgal@4.13.rb
@@ -5,7 +5,7 @@ class CgalAT413 < Formula
   sha256 "3e3dd7a64febda58be54c3cbeba329ab6a73b72d4d7647ba4931ecd1fad0e3bc"
 
   bottle do
-    root_url "https://dl.bintray.com/danston/bottles/cgal/4.13"
+    root_url "https://dl.bintray.com/danston/bottles"
     cellar :any
     rebuild 1
     sha256 "a608b3ae8c28753d3eb9eb935ea1f32588b7d0018dd7bac2b99d05b4fcd9041a" => :mojave
