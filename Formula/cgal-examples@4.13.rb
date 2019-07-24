023aabdf8923dfa1f5d9c4be61d1509a819a83ff
Dmitry Anisimov <danston@ymail.com>
2019-07-24 10:01:39 +0200

    update

--- Formula/cgal-examples@4.13.rb
+++ Formula/cgal-examples@4.13.rb
@@ -1,5 +1,5 @@
 class CgalExamplesAT413 < Formula
-  desc "Computational Geometry Algorithms Library: Examples Version 4.13"
+  desc "Computational Geometry Algorithms Library: Examples v.4.13"
   homepage "https://www.cgal.org/"
   url "https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.13/CGAL-4.13.tar.xz"
   sha256 "3e3dd7a64febda58be54c3cbeba329ab6a73b72d4d7647ba4931ecd1fad0e3bc"
@@ -8,20 +8,20 @@ class CgalExamplesAT413 < Formula
 
   depends_on "cmake" => [:build, :test]
   depends_on "boost"
-  depends_on "eigen"
-  depends_on "gmp"
-  depends_on "mpfr"
-  depends_on "qt"
-  depends_on "tbb"
-  depends_on "zlib"
-  depends_on "suite-sparse"
-  depends_on "open-mesh"
-  depends_on "mpfi"
-  depends_on "metis"
-  depends_on "vtk"
-  depends_on "opencv@2"
   depends_on "danston/cgal/esbtl"
   depends_on "danston/cgal/lastools"
+  depends_on "eigen"
+  depends_on "gmp"
+  depends_on "metis"
+  depends_on "mpfi"
+  depends_on "mpfr"
+  depends_on "open-mesh"
+  depends_on "opencv@2"
+  depends_on "qt"
+  depends_on "suite-sparse"
+  depends_on "tbb"
+  depends_on "vtk"
+  depends_on "zlib"
 
   def install
     args = std_cmake_args + %W[
@@ -35,28 +35,25 @@ class CgalExamplesAT413 < Formula
       -DOpenCV_DIR=/usr/local/opt/opencv@2/share/OpenCV
     ]
 
-    FileUtils.cp_r "examples/.", "#{prefix}/"
+    cp_r "examples/.", "#{prefix}/"
     system "cmake", ".", *args
     system "make", "install"
     system "ctest", "-E", "execution___of__"
-    
+
     puts "\nInstalling examples for CGAL 4.13..."
-    (Dir.entries("examples/") - [".", ".."]).each{|dirname| 
-      
-      if File.directory?("examples/#{dirname}/") then
-        (Dir.entries("examples/#{dirname}/") - [".", ".."]).each{|filename| 
+    (Dir.entries("examples/") - [".", ".."]).each do |dirname|
+      next unless File.directory?("examples/#{dirname}/")
 
-          if File.file?("examples/#{dirname}/#{filename}") then
-            extension = File.extname("examples/#{dirname}/#{filename}")
+      (Dir.entries("examples/#{dirname}/") - [".", ".."]).each do |filename|
+        next unless File.file?("examples/#{dirname}/#{filename}")
 
-            if extension == "" && filename != "Makefile" && filename != "skip_vcproj_auto_generation"
-              puts "examples/#{dirname}/#{filename}"
-              FileUtils.cp "examples/#{dirname}/#{filename}", "#{prefix}/#{dirname}/"
-            end
-          end
-        }
+        extension = File.extname("examples/#{dirname}/#{filename}")
+        if extension == "" && filename != "Makefile" && filename != "skip_vcproj_auto_generation"
+          puts "examples/#{dirname}/#{filename}"
+          cp "examples/#{dirname}/#{filename}", "#{prefix}/#{dirname}/"
+        end
       end
-    }
+    end
   end
 
   test do
