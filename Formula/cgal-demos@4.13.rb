28a0b4c45edbdf4b85fe5ad4386779e3f03db0d1
Dmitry Anisimov <danston@ymail.com>
2019-07-24 10:45:17 +0200

    update

--- Formula/cgal-demos@4.13.rb
+++ Formula/cgal-demos@4.13.rb
@@ -41,20 +41,20 @@ class CgalDemosAT413 < Formula
     (Dir.entries("demo/") - [".", ".."]).each do |dirname|
       next unless File.directory?("demo/#{dirname}/")
 
-        (Dir.entries("demo/#{dirname}/") - [".", ".."]).each do |filename|
-          next unless File.file?("demo/#{dirname}/#{filename}")
+      (Dir.entries("demo/#{dirname}/") - [".", ".."]).each do |filename|
+        next unless File.file?("demo/#{dirname}/#{filename}")
 
-            extension = File.extname("demo/#{dirname}/#{filename}")
-            if extension == "" && filename != "Makefile" && filename != "skip_vcproj_auto_generation"
-              puts "demo/#{dirname}/#{filename}"
-              cp "demo/#{dirname}/#{filename}", "#{prefix}/#{dirname}/"
-            end
-
-            if extension == ".dylib"
-              puts "demo/#{dirname}/#{filename}"
-              cp "demo/#{dirname}/#{filename}", "#{lib}"
-            end
+        extension = File.extname("demo/#{dirname}/#{filename}")
+        if extension == "" && filename != "Makefile" && filename != "skip_vcproj_auto_generation"
+          puts "demo/#{dirname}/#{filename}"
+          cp "demo/#{dirname}/#{filename}", "#{prefix}/#{dirname}/"
         end
+
+        if extension == ".dylib"
+          puts "demo/#{dirname}/#{filename}"
+          cp "demo/#{dirname}/#{filename}", "#{lib}"
+        end
+      end
     end
   end
 
