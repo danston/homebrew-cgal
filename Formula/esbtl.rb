f5787e6b76168bfa2b621296a1fec9ba86f856ef
Dmitry Anisimov <danston@ymail.com>
2019-07-25 16:22:42 +0200

    update

--- Formula/esbtl.rb
+++ Formula/esbtl.rb
@@ -1,9 +1,9 @@
 class Esbtl < Formula
   desc "Easy Structural Biology Template Library"
-  homepage "http://esbtl.sourceforge.net"
+  homepage "https://esbtl.sourceforge.io/"
   url "https://github.com/danston/cgal-brew/raw/master/esbtl.tar.gz"
-  sha256 "7b728ae51419bc8cda918f4770e68ef08fd6053df2144af1cb052479633af9a9"
   version "1.0"
+  sha256 "7b728ae51419bc8cda918f4770e68ef08fd6053df2144af1cb052479633af9a9"
 
   def install
     include.install Dir["include/*"]
--- Formula/lastools.rb
+++ Formula/lastools.rb
@@ -2,8 +2,8 @@ class Lastools < Formula
   desc "LAS tools"
   homepage "https://rapidlasso.com/LAStools"
   url "https://github.com/danston/cgal-brew/raw/master/lastools.tar.gz"
-  sha256 "74849f068573825ba98e293f03985447130e824b2a7c8f1c6f8e2eddabf0a6db"
   version "1.0"
+  sha256 "74849f068573825ba98e293f03985447130e824b2a7c8f1c6f8e2eddabf0a6db"
 
   def install
     include.install Dir["LASlib/inc/*"]
