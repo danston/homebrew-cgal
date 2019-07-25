6b58b00324f922d3df65d5a931ca44d1d3009288
Dmitry Anisimov <danston@ymail.com>
2019-07-25 19:51:34 +0200

    librs formula

--- Formula/librs.rb
+++ Formula/librs.rb
@@ -5,6 +5,8 @@ class Librs < Formula
   version "3.1.0"
   sha256 "ddbaef00f6a54805d8decfc7c4532bae6df4ac52d014d386703c92bcfee554e7"
 
+  keg_only :versioned_formula
+
   depends_on "mpfi"
 
   def install
