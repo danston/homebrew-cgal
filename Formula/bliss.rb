b77aed12cbd97a0311ade1c496c22bee6f40704a
Dmitry Anisimov <danston@ymail.com>
2019-07-26 12:35:47 +0200

    update

--- Formula/bliss.rb
+++ Formula/bliss.rb
@@ -6,7 +6,26 @@ class Bliss < Formula
   sha256 "5bc47253666d1b9ce70364d548ed44f0aaf45121eba5eac7fdfff928f515ce41"
 
   def install
-    system "make", "install"
+    system "make"
+
+    puts "\nInstalling bliss..."
+    (Dir.entries("./") - [".", ".."]).each do |filename|
+      next unless File.file?("./#{filename}")
+
+      extension = File.extname("./#{filename}")
+
+      if extension == "" && filename != "Makefile" && filename != "Doxyfile"
+        bin.install "./#{filename}"
+      end
+
+      if extension == ".hh" || extension == ".cc" || extension == ".o" || extension == ".h"
+        include.install "./#{filename}"
+      end
+
+      if extension == ".a"
+        lib.install "./#{filename}"
+      end
+    end  
   end
 
   test do
