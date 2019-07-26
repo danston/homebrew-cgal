5a3633c72c23994e7c44649a8003dd39ca48ec03
Dmitry Anisimov <danston@ymail.com>
2019-07-26 12:10:04 +0200

    update

--- Formula/esbtl.rb
+++ Formula/esbtl.rb
@@ -3,7 +3,7 @@ class Esbtl < Formula
   homepage "https://esbtl.sourceforge.io/"
   url "https://github.com/danston/cgal-brew/raw/master/esbtl.tar.gz"
   version "1.0"
-  sha256 "7b728ae51419bc8cda918f4770e68ef08fd6053df2144af1cb052479633af9a9"
+  sha256 ""
 
   def install
     include.install Dir["include/*"]
