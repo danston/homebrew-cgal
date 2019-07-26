5930b5dba44f9bed3e5dad5635a79cde9005d656
Dmitry Anisimov <danston@ymail.com>
2019-07-26 12:20:24 +0200

    update

--- Formula/bliss.rb
+++ Formula/bliss.rb
@@ -3,7 +3,7 @@ class Bliss < Formula
   homepage "http://www.tcs.hut.fi/Software/bliss/"
   url "https://github.com/danston/cgal-brew/raw/master/bliss.tar.gz"
   version "0.73"
-  sha256 ""
+  sha256 "5bc47253666d1b9ce70364d548ed44f0aaf45121eba5eac7fdfff928f515ce41"
 
   def install
     system "make", "install"
--- Formula/esbtl.rb
+++ Formula/esbtl.rb
@@ -3,7 +3,7 @@ class Esbtl < Formula
   homepage "https://esbtl.sourceforge.io/"
   url "https://github.com/danston/cgal-brew/raw/master/esbtl.tar.gz"
   version "1.0"
-  sha256 ""
+  sha256 "9bbb4f3b36f5d51ed2afcbd3143bb0e5e5e035982d6aedc7fe497a36761f45db"
 
   def install
     include.install Dir["include/*"]
