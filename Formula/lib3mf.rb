486ccd7be883c885a0a589380295ed015abca6d9
Dmitry Anisimov <danston@ymail.com>
2019-07-26 13:42:00 +0200

    added lib3mf

--- Formula/lib3mf.rb
+++ Formula/lib3mf.rb
@@ -3,7 +3,7 @@ class Lib3mf < Formula
   homepage "https://github.com/3MFConsortium/lib3mf"
   url "https://github.com/danston/cgal-brew/raw/master/lib3mf.tar.gz"
   version "1.8.1"
-  sha256 ""
+  sha256 "55700bf3b6f8890eb2e8b9e379da954c335737271af27a9a194005b2042a83d2"
 
   depends_on "ossp-uuid"
 
@@ -12,7 +12,7 @@ class Lib3mf < Formula
       -DCMAKE_BUILD_TYPE=Release
     ]
 
-    system "cmake", "."
+    system "cmake", ".", *args
     system "make", "install"
   end
 
