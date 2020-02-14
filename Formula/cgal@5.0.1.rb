13b7f64a529276446ab76403aa0303cccd8b51ee
Dmitry Anisimov <danston@ymail.com>
2020-02-14 11:52:32 +0100

    added cgal501

---
+++ Formula/cgal@5.0.1.rb
@@ -0,0 +1,65 @@
+class CgalAT501 < Formula
+  desc "Computational Geometry Algorithms Library: v.5.0.1"
+  homepage "https://www.cgal.org/"
+  url "https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-5.0.1/CGAL-5.0.1.tar.xz"
+  sha256 "66021111fe536268d044e5e01bd26e691d7b493c217a1ca4d9427284dd4b2a02"
+
+  bottle do
+    cellar :any
+    sha256 "91b1ca6db31e4132b36c2bf2ef6b1ff812daa78d750fe83664f63b1d8713ce77" => :catalina
+    sha256 "91b1ca6db31e4132b36c2bf2ef6b1ff812daa78d750fe83664f63b1d8713ce77" => :mojave
+    sha256 "91b1ca6db31e4132b36c2bf2ef6b1ff812daa78d750fe83664f63b1d8713ce77" => :high_sierra
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "boost"
+  depends_on "eigen"
+  depends_on "gmp"
+  depends_on "mpfr"
+
+  def install
+    args = std_cmake_args + %W[
+      -DCMAKE_CXX_FLAGS='-std=c++14'
+      -DWITH_Eigen3=ON
+      -DWITH_LAPACK=ON
+      -DWITH_CGAL_Qt5=OFF
+      -DWITH_CGAL_ImageIO=OFF
+    ]
+
+    system "cmake", ".", *args
+    system "make", "install"
+  end
+
+  test do
+    # https://doc.cgal.org/latest/Algebraic_foundations/Algebraic_foundations_2interoperable_8cpp-example.html
+    (testpath/"surprise.cpp").write <<~EOS
+      #include <CGAL/basic.h>
+      #include <CGAL/Coercion_traits.h>
+      #include <CGAL/IO/io.h>
+      template <typename A, typename B>
+      typename CGAL::Coercion_traits<A,B>::Type
+      binary_func(const A& a , const B& b){
+          typedef CGAL::Coercion_traits<A,B> CT;
+          CGAL_static_assertion((CT::Are_explicit_interoperable::value));
+          typename CT::Cast cast;
+          return cast(a)*cast(b);
+      }
+      int main(){
+          std::cout<< binary_func(double(3), int(5)) << std::endl;
+          std::cout<< binary_func(int(3), double(5)) << std::endl;
+          return 0;
+      }
+    EOS
+    (testpath/"CMakeLists.txt").write <<~EOS
+      cmake_minimum_required(VERSION 3.1...3.13)
+      find_package(CGAL)
+      add_executable(surprise surprise.cpp)
+      target_link_libraries(surprise PRIVATE CGAL::CGAL)
+    EOS
+    system "cmake", "-L", "-DCMAKE_BUILD_RPATH=#{HOMEBREW_PREFIX}/lib", "-DCMAKE_PREFIX_PATH=#{prefix}", "."
+    system "cmake", "--build", ".", "-v"
+    assert_equal "15\n15", shell_output("./surprise").chomp
+  end
+end
