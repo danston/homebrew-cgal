ee285a1238f08075577aa9d7ced94b8d98309c31
Dmitry Anisimov <danston@ymail.com>
2020-12-30 12:40:12 +0100

    cgal demos 5.1.2

---
+++ Formula/cgal-demos@5.1.2.rb
@@ -0,0 +1,103 @@
+class CgalDemosAT512 < Formula
+  desc "Computational Geometry Algorithms Library: Demos v.5.1.2"
+  homepage "https://www.cgal.org/"
+  url "https://github.com/CGAL/cgal/releases/download/v5.1.2/CGAL-5.1.2.tar.xz"
+  sha256 "e499ad8199580fa979a5a6e431e667c22ca2892ebbb7981e699849060645341d"
+
+  keg_only :versioned_formula
+
+  depends_on "cmake" => [:build, :test]
+  depends_on "boost"
+  depends_on "cimg"
+  depends_on "danston/cgal/lastools"
+  depends_on "eigen"
+  depends_on "gmp"
+  depends_on "metis"
+  depends_on "mpfr"
+  depends_on "opencv@3"
+  depends_on "qt"
+  depends_on "tbb"
+  depends_on "vtk"
+  depends_on "zlib"
+
+  def install
+    args = std_cmake_args + %W[
+      -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON
+      -DCMAKE_INSTALL_RPATH=#{lib}
+      -DCMAKE_INSTALL_NAME_DIR=#{lib}
+      -DCMAKE_CXX_FLAGS='-std=c++14'
+      -DCMAKE_CXX_FLAGS='-DCGAL_DO_NOT_USE_BOOST_MP'
+      -DWITH_Eigen3=ON
+      -DWITH_demos=ON
+      -DBUILD_TESTING=ON
+      -DQt5_DIR=#{HOMEBREW_PREFIX}/opt/qt/lib/cmake/Qt5
+      -DOpenCV_DIR=#{HOMEBREW_PREFIX}/opt/opencv@3/share/OpenCV
+    ]
+
+    cp_r "demo/.", "#{prefix}/"
+    system "cmake", ".", *args
+    system "make", "install"
+    system "ctest", "--timeout", "30000", "-E", "execution___of__"
+
+    puts "\nInstalling demos for CGAL 5.1.2..."
+    (Dir.entries("demo/") - [".", ".."]).each do |dirname|
+      next unless File.directory?("demo/#{dirname}/")
+
+      (Dir.entries("demo/#{dirname}/") - [".", ".."]).each do |filename|
+        next unless File.file?("demo/#{dirname}/#{filename}")
+
+        extension = File.extname("demo/#{dirname}/#{filename}")
+
+        if extension == "" && filename != "Makefile" && filename != "skip_vcproj_auto_generation"
+          puts "demo/#{dirname}/#{filename}"
+          cp "demo/#{dirname}/#{filename}", "#{prefix}/#{dirname}/"
+        end
+
+        if extension == ".dylib"
+          puts "demo/#{dirname}/#{filename}"
+          cp "demo/#{dirname}/#{filename}", "#{lib}/"
+        end
+
+        if [".cpp", ".h"].include?(extension)
+          puts "demo/#{dirname}/#{filename}"
+          cp "demo/#{dirname}/#{filename}", "#{prefix}/#{dirname}/"
+        end
+      end
+
+      next unless dirname == "Polyhedron"
+
+      (Dir.entries("demo/#{dirname}/Plugins/") - [".", ".."]).each do |plugin|
+        (Dir.entries("demo/#{dirname}/Plugins/#{plugin}/") - [".", ".."]).each do |filename|
+          next unless File.file?("demo/#{dirname}/Plugins/#{plugin}/#{filename}")
+
+          extension = File.extname("demo/#{dirname}/Plugins/#{plugin}/#{filename}")
+
+          if [".so", ".cpp", ".h"].include?(extension)
+            puts "demo/#{dirname}/Plugins/#{plugin}/#{filename}"
+            cp "demo/#{dirname}/Plugins/#{plugin}/#{filename}", "#{prefix}/#{dirname}/Plugins/#{plugin}/"
+          end
+
+          if extension == ".dylib"
+            puts "demo/#{dirname}/Plugins/#{plugin}/#{filename}"
+            cp "demo/#{dirname}/Plugins/#{plugin}/#{filename}", "#{lib}/"
+          end
+        end
+      end
+
+      (Dir.entries("demo/#{dirname}/implicit_functions/") - [".", ".."]).each do |filename|
+        next unless File.file?("demo/#{dirname}/implicit_functions/#{filename}")
+
+        extension = File.extname("demo/#{dirname}/implicit_functions/#{filename}")
+
+        if extension == ".so"
+          puts "demo/#{dirname}/implicit_functions/#{filename}"
+          cp "demo/#{dirname}/implicit_functions/#{filename}", "#{prefix}/#{dirname}/implicit_functions/"
+        end
+      end
+    end
+  end
+
+  test do
+    system "true"
+  end
+end
